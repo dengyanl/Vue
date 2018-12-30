3357b51e0f485ec6404be5b2b4718d3997ce34be
DengYanLee <2894126711@qq.com>
2018-12-30 16:03:48 +0800

    提交完成~~~!!

---
+++ day06/webpack-study/main.js
@@ -0,0 +1,25 @@
+
+
+
+
+// 这是main.js文件   使我们项目的 js  的入口文件
+
+// 导入jquery    ***  ES6 中的 导入模块方法   ***
+
+import $ from 'jquery'
+
+
+
+
+//使用  import  语法,导入 css样式表
+import './src/css/index.css'
+
+
+
+// import './css/index/.less'
+// import './cssindex.scss'
+
+
+
+
+
